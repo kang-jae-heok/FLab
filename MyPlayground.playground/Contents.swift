dc5335436ea058bd54ccda946a5232be362daabb
kang-jae-heok <kjhabc8707@naver.com>
2023-11-10 21:26:38 +0900

    Overload, Override example

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func add(_ a: Double, _ b: Double, _ c: Double) -> Double {
+    return a + b + c
+}
+
+add(5.0, 5.0, 5.0)
+add(5, 5)
+
+class Language {
+    func greeting() {
+        print("")
+    }
+}
+
+class Korean: Language {
+    override func greeting() {
+        print("안녕하세요")
+    }
+}
+
+class English: Language {
+    override func greeting() {
+        print("hi")
+    }
+}
